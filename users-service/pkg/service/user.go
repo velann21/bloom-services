352b7a57c18443fb941d89d7e249eb569123821d
velann21 <velann21@gmail.com>
2021-06-23 18:48:57 +0200

    Added indendations

--- users-service/pkg/service/user.go
+++ users-service/pkg/service/user.go
@@ -34,6 +34,7 @@ func NewUserService(userRepo repository.UserRepoInterface) UserInterface {
 
 func (users UserService) CreateUser(ctx context.Context, data *requests.User) error {
 	logrus.Debug("Inside the CreateUser Service")
+
 	result, err := users.userRepo.GetUser(ctx, data.Email)
 	if err != nil {
 		if err.Error() == helpers.RedisNil {
@@ -41,6 +42,7 @@ func (users UserService) CreateUser(ctx context.Context, data *requests.User) er
 		}
 		return err
 	}
+
 	if result != nil {
 		return helpers.UserAlreadyExist
 	}
@@ -50,24 +52,29 @@ createUser:
 	if err != nil {
 		return err
 	}
+
 	err = users.userRepo.CreateUser(ctx, data.Email, userModel, UserExpirationTime)
 	if err != nil {
 		return err
 	}
+
 	logrus.Debug("Done the CreateUser Service")
 	return nil
 }
 
 func (users UserService) GetUser(ctx context.Context, email string) (*models.User, error) {
 	logrus.Debug("Inside the GetUser Service")
+
 	result, err := users.userRepo.GetUser(ctx, email)
 	if err != nil {
 		return nil, err
 	}
+
 	if result == nil {
 		// Todo: Check in the file system if not available then return the No Result Found
 		return nil, helpers.NoResultFound
 	}
+
 	userModel := &models.User{}
 	err = userModel.PopulateUser(result)
 	if err != nil {
